ea08cdefd54a7bebf546863274bb344addd473d2
Jared <jbeckersalax9@aol.com>
2021-12-12 11:32:21 -0500

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const main = document.getElementById("main");
 main.remove();
 
-let newHeader = document.createElement('h1')
+const newHeader = document.createElement('h1')
 document.body.append(newHeader);
-$0.id = 'victory'
+newHeader.id = 'victory'
+newHeader.textContent = 'is the champion'
+
 
 
 
